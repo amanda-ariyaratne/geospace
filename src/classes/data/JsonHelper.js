e4ba2b9664a71eb042b455d1a9e3f2e5bf30b11d
amanda-ariyaratne <aariyaratne@gmail.com>
2020-04-18 17:19:50 +0530

    Coordinate Header Pickers

--- src/classes/data/JsonHelper.js
+++ src/classes/data/JsonHelper.js
@@ -4,3 +4,19 @@ export const isJsonArray = (jsonObj) => {
   }
   return false;
 };
+
+export const getArrayAttributes = (row) => {
+  if (!isJsonArray(row)) {
+    throw new Error("Not a json array");
+  }
+  if (row.length === 0) {
+    throw new Error("The file does not contain any objects to display");
+  }
+  let columnObject = {};
+  for (let i = 0; i < row.length; i++) {
+    columnObject[`Column ${i}`] = row[i];
+  }
+  return columnObject;
+};
+
+export const getObjectAttributes = (row) => {};
--- src/classes/data/JsonHelper.test.js
+++ src/classes/data/JsonHelper.test.js
@@ -1,4 +1,4 @@
-import { isJsonArray } from "./JsonHelper";
+import { isJsonArray, getArrayAttributes } from "./JsonHelper";
 
 test("test isJsonArray method", () => {
   expect(isJsonArray([1, 2, 3])).toBeTruthy();
@@ -7,4 +7,26 @@ test("test isJsonArray method", () => {
   expect(isJsonArray({})).toBeFalsy();
   expect(isJsonArray({ item: [{ id: 1 }, { id: 2 }, { id: 3 }] })).toBeFalsy();
   expect(isJsonArray({ id: 1, name: "John" })).toBeFalsy();
+});
+
+test("test getArrayAttributes", () => {
+  expect(getArrayAttributes(["a", "b", "c"])).toEqual({
+    "0": "a",
+    "1": "b",
+    "2": "c",
+  });
+
+  expect(getArrayAttributes([1, 2, 3])).toEqual({
+    "0": 1,
+    "1": 2,
+    "2": 3,
+  });
+
+  expect(() => {
+    getArrayAttributes({});
+  }).toThrow("Not a json array");
+
+  expect(() => {
+    getArrayAttributes([]);
+  }).toThrow("The file does not contain any objects to display");
 });
