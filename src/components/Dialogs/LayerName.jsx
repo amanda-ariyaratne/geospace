8e4bae5b40fd0c408ce73315283daf3ab93ae70b
amanda-ariyaratne <aariyaratne@gmail.com>
2020-04-24 00:10:23 +0530

    upload by csv

--- src/components/Dialogs/LayerName.jsx
+++ src/components/Dialogs/LayerName.jsx
@@ -2,13 +2,8 @@ import React from "react";
 
 // material-ui
 import { Box, TextField, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({}));
 
 export default function OpacitySlider(props) {
-  const classes = useStyles();
-
   const handleChange = (event) => {
     props.setName(event.target.value);
   };
