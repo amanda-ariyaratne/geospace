352941109744c7d3c62b18da0c28b155402e8172
amanda-ariyaratne <aariyaratne@gmail.com>
2020-04-23 15:42:16 +0530

    hardcoded tooltip over object on hover

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
+// deck.gl
 import { StaticMap } from "react-map-gl";
 import DeckGL from "deck.gl";
+
+// redux
 import { useSelector } from "react-redux";
+
+// material-ui
+import Chip from "@material-ui/core/Chip";
 
 export default function Map() {
   const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+
+  const [hoverObject, setHoverObject] = useState({});
+
+  const _renderTooltip = () => {
+    console.log(hoverObject.x, hoverObject.y);
+    return (
+      hoverObject && (
+        <div
+          style={{
+            position: "absolute",
+            zIndex: 5,
+            pointerEvents: "none",
+            left: hoverObject.x,
+            top: hoverObject.y,
+          }}
+        >
+          <Chip label="Basic" />
+        </div>
+      )
+    );
+  };
+
+  const _onHover = (info, event) => {
+    if (info.object !== undefined) {
+      setHoverObject(info);
+    }
+  };
 
   // source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
 
@@ -22,11 +56,13 @@ export default function Map() {
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
       layers={layers}
+      onHover={_onHover}
     >
       <StaticMap
         mapboxApiAccessToken={MAPBOX_TOKEN}
         mapStyle={mapboxstyle.url}
       />
+      {_renderTooltip()}
     </DeckGL>
   );
 }
